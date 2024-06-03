b5dabb06a3a43d4f600ecb96e7613ee9b78db0c9
Muhammad Yousuf <official.yousufpathan@gmail.com>
2024-06-03 14:41:03 +0800

    Create automator-scripts.sh

---
+++ automator-scripts.sh
@@ -0,0 +1,5 @@
+# sketchybar-reload.app >>>> shortcut in skhd for running after created in automator.app
+/opt/homebrew/bin/sketchybar --reload
+
+# yabai-reload.app  >>>>> shortcut in skhd for running after created in automator.app
+/opt/homebrew/bin/yabai --restart-service
