12769e8ba1003ee27a33c28e6ac5df661bbdbc0d
Muhammad Yousuf <work.myousuf@gmail.com>
2024-06-03 10:32:05 +0800

    dotfiles commit

---
+++ sketchybar/colors.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+export WHITE=0xffffffff
+
+# -- Teal Scheme --
+# export BAR_COLOR=0xff001f30
+export ITEM_BG_COLOR=0xff003547
+export ACCENT_COLOR=0xff2cf9ed
+# export BAR_COLOR=0xff001f30
+#
+#
+# export ITEM_BG_COLOR=0xff001f30
+# export ACCENT_COLOR=0xff001f30
+
+
+
+# -- Gray Scheme --
+# export BAR_COLOR=0xff101314
+# export ITEM_BG_COLOR=0xff353c3f
+# export ACCENT_COLOR=0xff353c3f
+
+# export BAR_COLOR=0xff000000
+
+# -- Black and White --
+# export ITEM_BG_COLOR=0xff000000
+# export ACCENT_COLOR=0xffffffff
+
+
+# -- Purple Scheme --
+# export ITEM_BG_COLOR=0xff2b1c84
+# export ACCENT_COLOR=0xffeb46f9
+
+
+# -- Red Scheme ---
+# export ITEM_BG_COLOR=0xff591221
+# export ACCENT_COLOR=0xff591221
+
+
+# -- Blue Scheme --- 
+# export ITEM_BG_COLOR=0xff093aa8
+# export ACCENT_COLOR=0xff15bdf9
+
+
+# -- Green Scheme --
+# export ITEM_BG_COLOR=0xff008c39
+# export ACCENT_COLOR=0xff1dfca1
+
+
+# -- Orange Scheme --
+# export ITEM_BG_COLOR=0xff99440a
+# export ACCENT_COLOR=0xfff97716
+
+
+# -- Yellow Scheme --
+# export ITEM_BG_COLOR=0xff8e7e0a
+# export ACCENT_COLOR=0xfff7fc17
